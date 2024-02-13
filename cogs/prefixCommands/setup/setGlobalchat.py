f730cbe7900e4aee2a600ee0b7b52ab6b5b8a56c
lz20r <zhiyaonaiara@gmail.com>
2024-02-13 20:23:16 +0100

    Update setGlobalchat command to use momochannel parameter

--- cogs/prefixCommands/setup/setGlobalchat.py
+++ cogs/prefixCommands/setup/setGlobalchat.py
@@ -26,7 +26,7 @@ class setMgchat(commands.Cog):
 
     @commands.command(name='momochat', aliases=['momocg', 'mgchat'])
     @commands.has_permissions(administrator=True)
-    async def globalchat(self, ctx, action=None, channel: discord.TextChannel = None):
+    async def globalchat(self, ctx, action=None, momochannel: discord.TextChannel = None):
         momoprefixes = await self.bot.get_prefix(ctx.message)
         momoprefix = momoprefixes[0] if momoprefixes else None
 
@@ -42,21 +42,21 @@ class setMgchat(commands.Cog):
             embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar.url)           
 
         if action == "set":
-            if channel is None:
+            if momochannel is None:
                 embed = discord.Embed(  
-                    description="<:momowarn:1206682132311842878> Please specify a channel to set for Momo Global Chat." 
+                    description="<:momowarn:1206682132311842878> Please specify a momochannel to set for Momo Global Chat." 
                 )
                 embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar.url)  
                 await ctx.send(embed=embed) 
                 return
             
-            channel_id = channel.id if isinstance(channel, discord.TextChannel) else channel
+            momochannelid = momochannel.id if isinstance(momochannel, discord.TextChannel) else momochannel
 
             momochatconfig = self.loadmomochatconfig()
-            momochatconfig[str(ctx.guild.id)] = channel_id
+            momochatconfig[str(ctx.guild.id)] = momochannelid
             self.savemomochatconfig(momochatconfig)
             embed = discord.Embed( 
-                description=f"<:momostar:1206265916472692839> Successfully set {channel.mention} as the Momo Global Chat channel." 
+                description=f"<:momostar:1206265916472692839> Successfully set {momochannel.mention} as the Momo Global Chat channel." 
             ) 
             embed.set_thumbnail(url=ctx.guild.icon.url),
             embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar.url)            
@@ -65,16 +65,16 @@ class setMgchat(commands.Cog):
         elif action == "remove":
             momochatconfig = self.loadmomochatconfig()
             if str(ctx.guild.id) in momochatconfig:
-                removed_channel_id = momochatconfig.pop(str(ctx.guild.id))
+                momoremovedchannelid = momochatconfig.pop(str(ctx.guild.id))
                 self.savemomochatconfig(momochatconfig)
-                removed_channel = self.bot.get_channel(removed_channel_id)
-                if removed_channel:
-                    embed = discord.Embed( description=f"<:momostar:1206265916472692839> Successfully removed {removed_channel.mention} as the Momo Global Chat channel.")
+                momoremovedchannel = self.bot.get_channel(momoremovedchannelid)
+                if momoremovedchannel:
+                    embed = discord.Embed( description=f"<:momostar:1206265916472692839> Successfully removed {momoremovedchannel.mention} as the Momo Global Chat channel.")
                     embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar.url)             
                     await ctx.send(embed=embed)
 
                 else:
-                    embed = discord.Embed(description=f"<:momostar:1206265916472692839> Successfully removed channel with ID `{removed_channel_id}` as the Momo Global Chat channel.")
+                    embed = discord.Embed(description=f"<:momostar:1206265916472692839> Successfully removed channel with ID `{momoremovedchannelid}` as the Momo Global Chat channel.")
                     embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar.url)  
                     await ctx.send(embed=embed)
             else:
