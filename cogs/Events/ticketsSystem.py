84ca2c6c81553f9376c2a1e1afb2a8c7a396d162
lz20r <zhiyaonaiara@gmail.com>
2024-02-22 11:49:07 +0100

    Add ticket creation and management functionality

--- cogs/Events/ticketsSystem.py
+++ cogs/Events/ticketsSystem.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 import discord
 from discord.ext import commands 
 
@@ -15,26 +16,63 @@ class TicketSystem(commands.Cog):
             await channel.set_permissions(ctx.author, view_channel=True, send_messages=True)
             await channel.set_permissions(ctx.guild.default_role, view_channel=False)
             await channel.set_permissions(ctx.guild.me, view_channel=True, send_messages=True)
- 
-            embed = discord.Embed(description=f"{ctx.author.mention} Bienvenido a tu ticket.")
-            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url)
-            await channel.send(embed=embed)
-            embed = discord.Embed(description=f"Ticket creado en {channel.mention}.")
-            await ctx.send(embed=embed)
-            return channel
-        
-        if ctx.channel.name == "ticket":
-            await ctx.send("Ya tienes un ticket abierto.", delete_after=5)
-        else:
-            channel = await create_ticket()
-            await ctx.author.move_to(channel)
-            await ctx.message.delete()
             
+            if channel is None:
+                await ctx.send("No se pudo crear el ticket.", delete_after=5)
+                return
+            elif ctx.channel == channel:
+                await ctx.send("Ya tienes un ticket abierto.", delete_after=5)
+                return
+            else:
+                embed = discord.Embed(description=f"{ctx.author.mention} Bienvenido a tu ticket.")
+                embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url)
+                await channel.send(embed=embed)
+                embed = discord.Embed(description=f"Ticket created in {channel.mention} in {ctx.channel.category}")
+                await ctx.send(embed=embed)
+                return channel      
+        await ctx.message.delete() 
+        await create_ticket()
+        return 
+    
     @commands.command(name="close")
-    async def close(self, ctx):
+    async def close_ticket(self, ctx):
         await ctx.channel.delete()
-        await ctx.send("Ticket cerrado.")
+        await ctx.send(f"Ticket closed {ctx.author.mention}", delete_after=5)
         return
+    
+    @commands.command(name="closeall")
+    async def close_all_tickets(self, ctx):
+        for channel in ctx.guild.channels:
+            if channel.name.startswith(f"ticket {ctx.author.name}"): 
+                await channel.delete()
+        return
+    
+    @commands.command(name="closecategory")
+    async def close_category(self, ctx):
+        for channel in ctx.guild.channels:
+            if channel.category == ctx.channel.category:
+                await channel.delete()
+        return
+    
+    @commands.command(name="closeallcategory")
+    async def close_all_category(self, ctx):
+        for channel in ctx.guild.channels:
+            if channel.category == ctx.channel.category:
+                await channel.delete()
+        return
+    
+    @commands.command(name="logsticket")
+    async def logs_ticket(self, ctx):
+        channel_id = 1210022535635410944
+        target_channel = self.bot.get_channel(channel_id)
+        if target_channel:
+            embed = discord.Embed(
+                title=f'`Ticket Logs`',
+                description=f'User: **{ctx.author.mention}**\nTicket: **{ctx.channel.name}**\nCategory: **{ctx.channel.category}**')
+            await target_channel.send(embed=embed)
+        return
+    
+     
      
 async def setup(bot):
     await bot.add_cog(TicketSystem(bot))
